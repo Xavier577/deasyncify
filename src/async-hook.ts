b938fe800a5f02edd6d6f4756cb752bdceb481df
joseph <josephtsegen10@gmail.com>
2022-06-11 17:53:31 +0100

    fixed issue with jest and added tests for watch and watchAll methods

--- src/async-hook.ts
+++ src/async-hook.ts
@@ -1,7 +1,7 @@
 export default class AsyncHook {
   public static watch = async <T>(
     asyncObj: Promise<T> | (() => Promise<T>)
-  ): Promise<[T | null | undefined, any]> => {
+  ): Promise<[T | null, any]> => {
     try {
       let result =
         asyncObj instanceof Function ? await asyncObj() : await asyncObj;
@@ -12,29 +12,38 @@ export default class AsyncHook {
     }
   };
 
-  public static watchAll = async <T>(
-    asyncArr: Promise<T>[] | (() => Promise<T>)[]
-  ): Promise<[T[] | null | undefined, any]> => {
+  public static watchAll = async <J, T extends Awaited<J>>(
+    asyncArr: J[]
+  ): Promise<[T[] | null, any]> => {
     try {
-      let result = await Promise.all(
-        asyncArr.map((asyncObj) =>
-          asyncObj instanceof Function ? asyncObj() : asyncObj
-        )
+      const toBeResolvedPromises = asyncArr.map((asyncObj) =>
+        asyncObj instanceof Function ? asyncObj() : asyncObj
       );
+      const result = await Promise.all<T[]>(toBeResolvedPromises);
       return [result, null];
-    } catch (err) {
+    } catch (err: any) {
       return [null, err];
     }
   };
 
   public static watchSettled = async <T>(
     asyncArr: Promise<T>[] | (() => Promise<T>)[]
-  ): Promise<PromiseSettledResult<T>[]> => {
+  ): Promise<[PromiseFulfilledResult<T>[], PromiseRejectedResult[]]> => {
     let result = await Promise.allSettled(
       asyncArr.map((asyncObj) =>
         asyncObj instanceof Function ? asyncObj() : asyncObj
       )
     );
-    return result;
+
+    const allResolved: PromiseFulfilledResult<T>[] = [];
+
+    const allRejected: PromiseRejectedResult[] = [];
+
+    result.forEach((promise) => {
+      if (promise.status === "fulfilled") allResolved.push(promise);
+      else if (promise.status === "rejected") allRejected.push(promise);
+    });
+
+    return [allResolved, allRejected];
   };
 }
